a1b464058a75ead14f5699b514cfdc088fc22022
Dehlan <167357045+DehlanRaymos@users.noreply.github.com>
2026-07-22 00:19:28 -0400

    Adjusted app routing so the app uses the shell

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AppShell } from './pages/app-shell/app-shell';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import("./pages/app-shell/app-shell").then(c => c.AppShell),
+    title: 'Atlas',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import("./pages/home/home").then(c => c.Home)
+      }
+    ]
+  }
+];
