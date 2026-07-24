bf93f41904839a8a1be5c84d8addb508c902bc62
Dehlan <167357045+DehlanRaymos@users.noreply.github.com>
2026-07-23 23:36:09 -0400

    Added ng motion library for animations. To achieve this, I needed to downgrade angular version from 22 to 21. I animated the progress bar and percentage number to on page load

--- front-end/src/app/components/sidebar/project-link/progress-bar/progress-bar.ts
+++ front-end/src/app/components/sidebar/project-link/progress-bar/progress-bar.ts
@@ -1,16 +1,20 @@
-import { Component, input, OnInit } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgmMotionDirective, type TargetAndTransition, type Transition } from '@scripttype/ng-motion';
 
 @Component({
   selector: 'app-progress-bar',
-  imports: [],
+  imports: [NgmMotionDirective],
   templateUrl: './progress-bar.html',
   styleUrl: './progress-bar.scss',
 })
-export class ProgressBar implements OnInit {
+export class ProgressBar {
   public readonly Progress = input<number>(0);
   public readonly Color = input<string>('#fff');
 
-  ngOnInit() {
-    // Animation triggers on component load via motion library
+  public readonly Initial: TargetAndTransition = { opacity: 0, width: '0%' };
+  public readonly Transition: Transition = { duration: 0.8, ease: 'easeOut' };
+
+  public Animate(): TargetAndTransition {
+    return { opacity: 1, width: `${this.Progress()}%` };
   }
 }
