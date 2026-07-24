2a0ec128103bc8ccd6567cd3d38eb2c19bee4443
Dehlan <167357045+DehlanRaymos@users.noreply.github.com>
2026-07-23 23:19:14 -0400

    Fixed an issue where the project links wasn't inheriting the gap set in the nav links container. Since the project links are looped within the component, the nav link styles in the sidebar only views 1 child. I added the gap instead in the project link component

--- front-end/src/app/api-services/project-service.ts
+++ front-end/src/app/api-services/project-service.ts
@@ -8,8 +8,8 @@ import { from, Observable } from 'rxjs';
 export class ProjectService {
 	private MockProjects: IProjectLink[] = [
 		{ Id: 1, Name: 'Q3 AI Integration', Progress: 78, Color: '#20FF63', Href: '#' },
-		{ Id: 2, Name: 'Q3 Marketing Campaign', Progress: 58, Color: '#FF41DF', Href: '#' },
-		
+		{ Id: 2, Name: 'Q4 Marketing Campaign', Progress: 58, Color: '#FF41DF', Href: '#' },
+
 	]
 
 	public LoadProjectLinks(): Observable<IProjectLink[]> {
